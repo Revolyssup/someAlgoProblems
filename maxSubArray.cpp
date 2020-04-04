caf54186c0d7c436dd366562c4867e502a0f2201
Ashish <ashishjaitiwari15112000@gmail.com>
2020-04-05 03:49:13 +0530

    problem 1

---
+++ maxSubArray.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int accum=INT_MIN;
+        int mov=0;
+        int count=nums.size();
+        for(int i=0;i<count;i++){
+            mov+=nums[i];
+            if(mov>accum){
+                accum=mov;
+            }
+            if(mov<0){
+                mov=0;
+            }
+    } 
+        return accum;
+ }          
+};
