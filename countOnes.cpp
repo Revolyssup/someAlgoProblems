438c09129ed0ebf819124aa2855bc7cdd65ed8aa
Ashish <ashishjaitiwari15112000@gmail.com>
2020-05-31 01:48:19 +0530

    giveOnes added

---
+++ countOnes.cpp
@@ -0,0 +1,49 @@
+/* Basically you pass a positive integer n as argument, and giveOnes return an array 
+   counting number of ones in binary form of all the numbers in range [0,n].*/
+#include <iostream>
+#include<cmath>
+#include<vector>
+using namespace std;
+
+vector<int> giveOnes(int n)
+{
+        vector<int> ans;
+        int power;
+        int ones;
+        int temp;
+        for(int i=0;i<=n;i++)
+        {
+                ones=0;
+                power=0;
+                temp=i;
+                while(temp!=0){
+                        temp=temp/2;
+                        power++;
+
+                }
+                temp=i;
+                power=pow(2,power);
+                while(temp!=0){
+                        if(power<=temp){
+                                temp=temp%power;
+                                ones++;
+                        }
+                        power=power/2;
+                        
+                }
+                
+            ans.push_back(ones);        
+        }
+   return ans;        
+}
+
+
+int main()
+{
+    vector<int> answer=giveOnes(15);        
+    for(int i=0;i<answer.size();i++){
+            std::cout <<answer[i] <<" ";
+    }
+
+    return 0;
+}
