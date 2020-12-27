0665201977364bcdeee5e288f0be9202e90a958d
Ashish <ashishjaitiwari15112000@gmail.com>
2020-12-27 18:56:32 +0530

    Added problems

--- codeforces/692/fair.cpp
+++ codeforces/692/fair.cpp
@@ -2,12 +2,12 @@
 
 
 //pending
-int main(){
-    int n,t;
-    std::cin>>t;
-    for(int i=0;i<t;i++){
-        std::cin>>n;
-        std::cout<<fair(n)<<std::endl;
-    }
-    return 0;
-}
+// int main(){
+//     int n,t;
+//     std::cin>>t;
+//     for(int i=0;i<t;i++){
+//         std::cin>>n;
+//         std::cout<<fair(n)<<std::endl;
+//     }
+//     return 0;
+// } 
