01aca9f4eb9f2c2b15319fa0fb8a4647e3423eb0
Ashish test <ashishjaitiwari15112000@gmail.com>
2021-05-28 16:23:48 +0530

    fixed lis

--- leetcode/lis.cpp
+++ leetcode/lis.cpp
@@ -7,25 +7,27 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         vector<int> ans(nums.size());
+        int toReturn=1;
         ans[0]=1;
-        for(int i=1;i<ans.size();i++){
+        for(int i=1;i<nums.size();i++){
             int max=0;
-            for(int j=i-1;j>-1;j--){
+            for(int j=i-1;j>=0;j--){
 
                 if(nums[j]<nums[i] && ans[j]>max){
                     max=ans[j];
                 } 
             }
+         
             ans[i]=max+1;
+            if(ans[i]>toReturn) toReturn=ans[i];
         }
-        return ans[ans.size()-1];
-        
+        return toReturn;
     }
 };
 
 int main(){
 
-    vector<int> nums={7,7,7,7,7,7,7};
+    vector<int> nums={1,3,6,7,9,4,10,5,6};
     Solution sol;
     cout<<sol.lengthOfLIS(nums)<<"\n";
 }
